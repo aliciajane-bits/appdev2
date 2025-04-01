456e0d543c6d50611059313e2f1acdc5341f5fbd
aliciajane-bits <aliciajanemedina881@gmail.com>
2025-04-01 13:12:57 +0800

    Laboratory Exercise 4

---
+++ url.js
@@ -0,0 +1,8 @@
+const url = require('url');
+
+const myUrl = 'http://www.example.com:8080/pathname?name=JohnDoe#fragment';
+
+const parsedUrl = url.parse(myUrl, true);
+console.log(parsedUrl);
+
+console.log('Name Parameter:', parsedUrl.query.name);
